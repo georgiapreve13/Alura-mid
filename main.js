62130df4d49cecf81f50e928a62bc985c9c9f1c9
geórgia preve <110172471+georgiapreve13@users.noreply.github.com>
2023-10-23 07:58:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-alert('Olá mundo');
+//Pom ---------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom'). play();
+}
+
+const listaDeTeclas = Document.querySelectorll ('.tecla');
+
+
+//enquanto
+white(){
+    listaDeTeclas[0].onclik = tocaSomPom
+}
+listaDeTeclas[]
 
